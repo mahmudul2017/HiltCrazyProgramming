b4386be239dbaab77c37455a4d2ca7e0cd92abde
mahmudul2017 <mahmudulhasanshah@gmail.com>
2021-09-19 18:48:38 +0600

    + Splash layout added

---
+++ app/src/main/java/com/hilt/crazyprogramming/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.hilt.crazyprogramming.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hilt.crazyprogramming.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
